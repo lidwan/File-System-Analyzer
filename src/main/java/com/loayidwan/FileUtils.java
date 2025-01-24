e9bb1816540e0aed55c161dea9059abef1340cdd
lidwan <contact@loayidwan.com>
2025-01-24 23:45:59 +0300

    Replaced manual extension extraction with library

--- src/main/java/com/loayidwan/FileUtils.java
+++ src/main/java/com/loayidwan/FileUtils.java
@@ -1,8 +1,10 @@
 package com.loayidwan;
+import org.apache.commons.io.FilenameUtils;
 
 public class FileUtils {
     public static String getFileExtension(String fileName) {
-        int lastDotIndex = fileName.lastIndexOf(".");
-        return (lastDotIndex == -1) ? "unknown" : fileName.substring(lastDotIndex + 1);
+        String ext = FilenameUtils.getExtension(fileName);
+
+        return ext.isEmpty() ? "Unknown" : ext;
     }
 }

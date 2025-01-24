6cc9e05c52142979bad4c395d57a624332e66f84
lidwan <contact@loayidwan.com>
2025-01-24 15:02:27 +0300

    Added user specified dict.

--- src/main/java/com/loayidwan/FileSystemAnalyzer.java
+++ src/main/java/com/loayidwan/FileSystemAnalyzer.java
@@ -1,8 +1,18 @@
 package com.loayidwan;
 
+import java.util.Scanner;
+
 public class FileSystemAnalyzer {
     public static void main(String[] args) {
-        FileScanner scanner = new FileScanner("src/main/resources");
-        scanner.scan();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Choose dir to analyze (Press Enter for default 'src/main/resources'): ");
+
+        String dict = scanner.nextLine();
+        if (dict.isEmpty())
+            dict = "src/main/resources";
+
+        FileScanner fileScanner = new FileScanner(dict);
+        fileScanner.scan();
     }
 }

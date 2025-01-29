14d8b0eac2eddf29ec4b856b3730b876f571182c
lidwan <contact@loayidwan.com>
2025-01-29 12:41:27 +0300

    Added delete duplicate files test

--- src/main/java/com/loayidwan/FileScanner.java
+++ src/main/java/com/loayidwan/FileScanner.java
@@ -3,6 +3,7 @@ package com.loayidwan;
 import java.io.IOException;
 import java.nio.file.*;
 import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
 
 
 public class FileScanner {
@@ -22,12 +23,31 @@ public class FileScanner {
                         fileStats.addFile(filePath);
                     } catch (IOException | NoSuchAlgorithmException e) {
                         throw new RuntimeException(e);
+                    } catch (SecurityException e){
+                        System.err.println(e.getMessage());
                     }
                 }
             });
+
             //tmp ofc, just for dev.
             fileStats.makeResFile(dictPath);
-
+            System.out.println("Do you want to delete duplicate files? (y, n)");
+            Scanner scanner = new Scanner(System.in);
+            String deleteduplFilesAnswer = "";
+            
+            while(deleteduplFilesAnswer.isEmpty())
+                deleteduplFilesAnswer = scanner.nextLine();
+            
+            if (deleteduplFilesAnswer.toLowerCase().charAt(0) == 'y'){
+                try {
+                    fileStats.deleteDuplFiles();
+                } catch (SecurityException e) {
+                    System.err.println(e.getMessage());
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+                fileStats.getDeletedFiles();
+            }
         } catch (Exception e) {
             System.err.println("Error traversing directory: " + e.getMessage());
         }

4f7ea84b3fa9cdc3ecf15aef5af43dadd466c737
lidwan <contact@loayidwan.com>
2025-02-03 19:01:28 +0300

    Implemented a control class for welcome scene

---
+++ src/main/java/com/loayidwan/WelcomeSceneController.java
@@ -0,0 +1,33 @@
+package com.loayidwan;
+
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+
+
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+
+public class WelcomeSceneController {
+
+    public void switchToScene2(javafx.event.ActionEvent event) throws IOException {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/filePickerScene.fxml"));
+            Parent root = loader.load();
+
+            Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace(); // Prints exact error
+        }
+
+    }
+}

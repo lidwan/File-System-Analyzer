1ab0eda6972dd370ff759cfaeee5d3ffe50f0ce5
lidwan <contact@loayidwan.com>
2025-02-03 16:53:17 +0300

    Implemented the basic welcome scene in javaFX

--- src/main/java/com/loayidwan/FileSystemAnalyzerGUI.java
+++ src/main/java/com/loayidwan/FileSystemAnalyzerGUI.java
@@ -1,24 +1,29 @@
 package com.loayidwan;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class FileSystemAnalyzerGUI extends Application {
 
     @Override
-    public void start(Stage stage) {
-        VBox root = new VBox(new Label("JavaFX is working!"));
-        Scene scene = new Scene(root, 320, 240);
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/welcomeScene.fxml")));
+        Scene scene = new Scene(root);
         stage.setScene(scene);
-        stage.setTitle("Test");
+        stage.setTitle("File System Analyzer | Loay Idwan");
+        String cssFile = getClass().getResource("/FileSystemAnalyzerStyles.css").toExternalForm();
+        scene.getStylesheets().add(cssFile);
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 
 }

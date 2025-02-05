895c8457c684460a6a12daef77f9e61aeb700954
lidwan <contact@loayidwan.com>
2025-02-05 14:10:35 +0300

    Cleaned up code

--- src/main/java/com/loayidwan/WelcomeSceneController.java
+++ src/main/java/com/loayidwan/WelcomeSceneController.java
@@ -1,20 +1,25 @@
 package com.loayidwan;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
 
 
-import java.awt.event.ActionEvent;
 import java.io.IOException;
 
 public class WelcomeSceneController {
+
+    public Hyperlink githubLink;
+    public Hyperlink wevsiteLink;
+    public Hyperlink blogLink;
 
     public void switchToScene2(javafx.event.ActionEvent event) throws IOException {
         try {
@@ -32,5 +37,14 @@ public class WelcomeSceneController {
             alert.showAndWait();
         }
 
+    }
+
+    public void handelGithubLink(ActionEvent actionEvent) {
+    }
+
+    public void handelBlogLink(ActionEvent actionEvent) {
+    }
+
+    public void handelWebsiteLink(ActionEvent actionEvent) {
     }
 }

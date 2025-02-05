cf10326f19750f6081e02e0dd7c94613ee3d254b
lidwan <contact@loayidwan.com>
2025-02-05 17:07:23 +0300

    Removed handling links

--- src/main/java/com/loayidwan/WelcomeSceneController.java
+++ src/main/java/com/loayidwan/WelcomeSceneController.java
@@ -1,25 +1,14 @@
 package com.loayidwan;
 
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
-import javafx.scene.control.Hyperlink;
-import javafx.scene.control.Label;
-import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
-
-
 import java.io.IOException;
 
 public class WelcomeSceneController {
-
-    public Hyperlink githubLink;
-    public Hyperlink wevsiteLink;
-    public Hyperlink blogLink;
 
     public void switchToScene2(javafx.event.ActionEvent event) throws IOException {
         try {
@@ -36,15 +25,5 @@ public class WelcomeSceneController {
             Alert alert = new Alert(Alert.AlertType.ERROR, "Error loading scene: " + e.getMessage());
             alert.showAndWait();
         }
-
-    }
-
-    public void handelGithubLink(ActionEvent actionEvent) {
-    }
-
-    public void handelBlogLink(ActionEvent actionEvent) {
-    }
-
-    public void handelWebsiteLink(ActionEvent actionEvent) {
     }
 }
